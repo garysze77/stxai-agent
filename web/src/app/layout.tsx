fad840bab1fc53e96ce1aa54383ac2c38283c824
Gary Sze <garysze77@gmail.com>
2026-05-09 14:30:47 +0800

    Remove AdSense script, keep meta tag only

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/components/AuthProvider";
@@ -34,12 +33,6 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9811241980847307"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
